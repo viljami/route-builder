afd2e1cf5dc771dc5c66d871736bd7026230bc85
Viljami Peltola <viljami.peltola@iki.fi>
2020-08-24 13:15:26 +0300

    Add docs for latest version.

---
+++ build/precache-manifest.515c700d9deb787c57d7027367ce480a.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "517d497ecea9d7f8c3c5e4b63e5c4429",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2404c208803dd595c0d",
+    "url": "/static/css/2.ed3cbb2b.chunk.css"
+  },
+  {
+    "revision": "063795608f15dae7b8fc",
+    "url": "/static/css/main.74164237.chunk.css"
+  },
+  {
+    "revision": "c2404c208803dd595c0d",
+    "url": "/static/js/2.91d5d210.chunk.js"
+  },
+  {
+    "revision": "b3ccc4efc5e11906002acb6f5fd2d702",
+    "url": "/static/js/2.91d5d210.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "063795608f15dae7b8fc",
+    "url": "/static/js/main.773b645c.chunk.js"
+  },
+  {
+    "revision": "1727523727b686267597",
+    "url": "/static/js/runtime-main.13ad7134.js"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/static/media/fa-solid-900.ee09ad75.woff"
+  }
+]);
